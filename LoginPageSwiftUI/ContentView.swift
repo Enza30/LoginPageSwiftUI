4a57d09a079ed1e656cea1c7c974ec3e36123ef2
Farendza Muda <farendza.muda@gmail.com>
2022-09-06 07:15:22 +0700

    Initial Commit

---
+++ LoginPageSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginPageSwiftUI
+//
+//  Created by Farendza Muda on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
